bca26063cf9e8ee583f85e0d7c6d5b11b65a8abe
Don French <dhfrench@vertisoft.com>
2019-06-24 18:33:10 -0700

    Get and Find DiagnosticREports

--- diagnosticreport.go
+++ diagnosticreport.go
@@ -4,19 +4,52 @@ import (
 	"encoding/json"
 	"fmt"
 	"time"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 // GetDiagnosticReport will return a diagnostic report for a patient with id pid
-func (c *Connection) GetDiagnosticReport(pid string) (*DiagnosticReport, error) {
-	fmt.Printf("DiagnosticReport?patient=%v", pid)
-	b, err := c.Query(fmt.Sprintf("DiagnosticReport?patient=%v", pid))
+func (c *Connection) FindDiagnosticReport(query string) (*DiagnosticReport, error) {
+	fmt.Printf("\n\nFindDiagnosticReport : DiagnosticReport?%v\n", query)
+	b, err := c.Query(fmt.Sprintf("DiagnosticReport?%s", query))
+	//fmt.Printf("GetDiag error: %v\n", err)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("@@@DiagnosticReport:\n")
-	spew.Dump(b)
+	// fmt.Printf("\n\n\n@@@ RAW DiagnosticReport: %s\n\n\n", pretty.Pretty(b))
+	// spew.Dump(b)
+	data := DiagnosticReport{}
+	if err := json.Unmarshal(b, &data); err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+// GetDiagnosticReport will return a diagnostic report for a patient with id pid
+func (c *Connection) GetDiagnosticReport(id string) (*DiagnosticReport, error) {
+	fmt.Printf("Get:  DiagnosticReport/%s\n", id)
+	b, err := c.Query(fmt.Sprintf("DiagnosticReport/%s", id)) //?patient=%v", pid))
+	//fmt.Printf("GetDiag error: %v\n", err)
+	if err != nil {
+		return nil, err
+	}
+	// fmt.Printf("\n\n\n@@@ RAW DiagnosticReport: %s\n\n\n", pretty.Pretty(b))
+	// spew.Dump(b)
+	data := DiagnosticReport{}
+	if err := json.Unmarshal(b, &data); err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+// GetPatientDiagnosticReport will return a diagnostic report for a patient with id pid
+func (c *Connection) GetPatientDiagnosticReport(pid string) (*DiagnosticReport, error) {
+	fmt.Printf("GetPat DiagnosticReport/%s\n", pid)
+	b, err := c.Query(fmt.Sprintf("DiagnosticReport/?patient=%d", pid)) //, ", pid))
+	if err != nil {
+		return nil, err
+	}
+	// fmt.Printf("\n\n\n@@@ RAW DiagnosticReport: %s\n\n\n", pretty.Pretty(b))
+	// spew.Dump(b)
 	data := DiagnosticReport{}
 	if err := json.Unmarshal(b, &data); err != nil {
 		return nil, err
@@ -27,68 +60,44 @@ func (c *Connection) GetDiagnosticReport(pid string) (*DiagnosticReport, error) 
 // DiagnosticReport is a FHIR report
 type DiagnosticReport struct {
 	SearchResult
-	Entry []ResourceEntry `json:"entry"`
-
-	// Entry []struct {
-	// 	EntryPartial
-	// 	Resource struct {
-	// 		ResourcePartial
-	// 		EffectiveDateTime time.Time    `json:"effective_date_time"`
-	// 		Issued            time.Time    `json:"issued"`
-	// 		Identifier        []Identifier `json:"identifier"`
-	// 		Meta              MetaData     `json:"meta"`
-	// 		Text              TextData     `json:"text"`
-	// 		Category          CodeText     `json:"category"`
-	// 		Code              CodeText     `json:"code"`
-	// 		PresentedForm     []Attachment `json:"presentedForm"`
-	// 		Request           []Thing      `json:"request"`
-	// 		Encounter         Encounter    `json:"encounter"`
-	// 		Result            []Thing      `json:"result"`
-	// 	} `json:"resource"`
-	// } `json:"entry"`
+	Entry []struct {
+		EntryPartial
+		Resource struct {
+			ResourcePartial
+			Encounter         Thing        `json:"encounter"`
+			EffectiveDateTime time.Time    `json:"effective_date_time"`
+			Issued            time.Time    `json:"issued"`
+			Identifier        []Identifier `json:"identifier"`
+			Meta              MetaData     `json:"meta"`
+			Text              TextData     `json:"text"`
+			Category          Category     `json:"category"`
+			Code              Note         `json:"code"`
+			PresentedForm     []Attachment `json:"presentedForm"`
+			Request           []Thing      `json:"request"`
+			Result            []Thing      `json:"result"`
+		} `json:"resource"`
+	} `json:"entry"`
 }
 
-type ResourceEntry struct {
-	EntryPartial EntryPartial
-	Resource     DiagnosticReportResource `json:"resource"`
-}
+// type ResourceEntry struct {
+// 	EntryPartial EntryPartial
+// 	Resource     DiagnosticReportResource `json:"resource"`
+// }
 
-type DiagnosticReportResource struct {
-	ResourcePartial   ResourcePartial
-	EffectiveDateTime time.Time    `json:"effective_date_time"`
-	Issued            time.Time    `json:"issued"`
-	Identifier        []Identifier `json:"identifier"`
-	Meta              MetaData     `json:"meta"`
-	Text              TextData     `json:"text"`
-	Category          CodeText     `json:"category"`
-	Code              CodeText     `json:"code"`
-	PresentedForm     []Attachment `json:"presentedForm"`
-	Request           []Thing      `json:"request"`
-	Encounter         Encounter    `json:"encounter"`
-	Result            []Thing      `json:"result"`
-}
-
-//Category the DiagnosticReport Category
-type Category struct {
-	Text string `json:"text"`
-}
-
-//Encounter of the report
-type Encounter struct {
-	Reference string `json:"reference"`
-}
-
-//MetaData meta field in DiagnosticReport
-type MetaData struct {
-	VersionID   string    `json:"versionId"`
-	LastUpdated time.Time `json:"lastUpdated"`
-}
-
-//TextData is the text of a diagonistic report
-type TextData struct {
-	Status string `json:"status"`
-	Div    string `json:"div"`
-}
+// type DiagnosticReportResource struct {
+// 	ResourcePartial   ResourcePartial
+// 	EffectiveDateTime time.Time    `json:"effective_date_time"`
+// 	Issued            time.Time    `json:"issued"`
+// 	Identifier        []Identifier `json:"identifier"`
+// 	Meta              MetaData     `json:"meta"`
+// 	Text              TextData     `json:"text"`
+// 	Category          CodeText     `json:"category"`
+// 	Code              CodeText     `json:"code"`
+// 	PresentedForm     []Attachment `json:"presentedForm"`
+// 	Request           []Thing      `json:"request"`
+// 	Encounter         Encounter    `json:"encounter"`
+// 	Result            []Thing      `json:"result"`
+// }
 
 // // Return the actual decoded text attachment
 // func (a *Attachment) DecodeImage() (string, error) {
